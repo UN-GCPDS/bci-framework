db5fce92b1b7ad8ed0d6162db1d70d1608bb8fd0
Yeison <yeison.eng@gmail.com>
2020-06-17 16:54:47 -0500

    Integrated Acquisition and Records features

---
+++ transformers/record.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from openbci_stream.handlers import HDF5_Writer
+from bci_framework.projects import properties as prop
+from bci_framework.projects.utils import loop_consumer, fake_loop_consumer
+
+import signal
+import atexit
+import os
+
+
+########################################################################
+class RecordTransformer:
+    """"""
+
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+
+        now = datetime.now()
+
+        filename = now.strftime('%x%X').replace('/', '_').replace(':', '_')
+
+        self.writer = HDF5_Writer(os.path.join(
+            'records', f'record-{filename}.h5'))
+        # self.writer = HDF5_Writer(f'{filename}.h5')
+
+        header = {'sample_rate': prop.SAMPLE_RATE,
+                  'datetime': now.timestamp(),
+                  'montage': prop.MONTAGE_NAME,
+                  'channels': prop.MONTAGE,
+                  }
+        self.writer.add_header(header)
+
+        signal.signal(signal.SIGINT, self.stop)
+        signal.signal(signal.SIGTERM, self.stop)
+        atexit.register(self.stop)
+
+        self.save_data()
+
+    # ----------------------------------------------------------------------
+    # @loop_consumer
+
+    @fake_loop_consumer
+    def save_data(self, data):
+        """"""
+        # self.writer.add_eeg(data.value['data'], data.timestamp / 1000)
+        self.writer.add_eeg(data.T, datetime.now().timestamp())
+
+    # ----------------------------------------------------------------------
+    def stop(self, *args, **kwargs):
+        """"""
+        self.writer.close()
+
+        with open('good.txt', 'w') as file:
+            file.write('good')
+
+
+if __name__ == '__main__':
+    RecordTransformer()
+
