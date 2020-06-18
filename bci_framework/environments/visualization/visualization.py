f5ff550447c66a94ccc678f944d74d443b60af58
Yeison <yeison.eng@gmail.com>
2020-06-18 11:58:37 -0500

    Removed records handler (and added to another script)

--- bci_framework/environments/visualization/visualization.py
+++ bci_framework/environments/visualization/visualization.py
@@ -1,11 +1,16 @@
-from PySide2.QtCore import QTimer
+# from PySide2.QtCore import QTimer
 from .visualization_widget import VisualizationWidget
 
-from datetime import datetime
-import time
+# # from datetime import datetime
+# # import time
+# # import sys
+# # import os
 
+# # from bci_framework.subprocess_script import run_subprocess
 
 ########################################################################
+
+
 class Visualization:
     """"""
 
@@ -24,36 +29,37 @@ class Visualization:
         """"""
         self.parent.pushButton_add_visualizarion.clicked.connect(
             self.add_subwindow)
-        self.parent.pushButton_record.toggled.connect(self.record_signal)
+        # self.parent.pushButton_record.toggled.connect(self.record_signal)
+
+    # # ----------------------------------------------------------------------
+    # def record_signal(self, toggled):
+        # """"""
+        # if toggled:
+            # self.start_record = datetime.now()
+            # self.timer = QTimer()
+            # self.timer.setInterval(1000 / 1)
+            # self.timer.timeout.connect(self.update_timer)
+            # self.timer.start()
+            # self.subprocess_script = run_subprocess(
+                # [sys.executable, os.path.join('transformers', 'record.py')])
+
+        # else:
+            # self.timer.stop()
+            # self.parent.pushButton_record.setText(f"Record")
+            # self.subprocess_script.kill()
+
+    # # ----------------------------------------------------------------------
+    # def update_timer(self):
+        # """"""
+        # now = datetime.now()
+        # delta = now - self.start_record
+
+        # n_time = datetime.strptime(str(delta), '%H:%M:%S.%f').time()
+
+        # self.parent.pushButton_record.setText(
+            # f"Recording [{n_time.strftime('%H:%M:%S')}]")
 
     # ----------------------------------------------------------------------
-    def record_signal(self, toggled):
-        """"""
-        if toggled:
-            self.start_record = datetime.now()
-            self.timer = QTimer()
-            self.timer.setInterval(1000 / 1)
-            self.timer.timeout.connect(self.update_timer)
-            self.timer.start()
-        else:
-
-            self.timer.stop()
-            self.parent.pushButton_record.setText(f"Record")
-
-    # ----------------------------------------------------------------------
-
-    def update_timer(self):
-        """"""
-        now = datetime.now()
-        delta = now - self.start_record
-
-        n_time = datetime.strptime(str(delta), '%H:%M:%S.%f').time()
-
-        self.parent.pushButton_record.setText(
-            f"Recording [{n_time.strftime('%H:%M:%S')}]")
-
-    # ----------------------------------------------------------------------
-
     def add_subwindow(self):
         """"""
 
