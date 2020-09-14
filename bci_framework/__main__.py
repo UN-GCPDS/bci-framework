0d047769f445ad91714166ab542424d0ef7e8d60
Yeison <yeison.eng@gmail.com>
2020-09-13 23:12:32 -0500

    Add --debug option

--- bci_framework/__main__.py
+++ bci_framework/__main__.py
@@ -36,39 +36,31 @@ def main():
 
     os.environ['BCISTREAM_DPI'] = str(app.screens()[0].physicalDotsPerInch())
 
-    pixmap = QPixmap("assets/splash_curves.svg")
-    splash = QSplashScreen(
-        pixmap, Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
-    splash.setMask(pixmap.mask())
-    splash.show()
-    splash.setStyleSheet("""
-        font-family: mono;
-        font-weight: normal;
-        font-size: 11pt;
-        """)
-
-    # def splash_logging(msg):
-    # if not splash is None:
-    # splash.showMessage(
-    # '\t' + msg, color=QColor("#888e93"), alignment=Qt.AlignBottom)
-    # splash_logging(f'eeg-framework 0.1')
+    if '--no_splash' in sys.argv:
+        pixmap = QPixmap(os.path.join("assets", "splash_curves.svg"))
+        splash = QSplashScreen(
+            pixmap, Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+        splash.setMask(pixmap.mask())
+        splash.show()
+        splash.setStyleSheet("""
+            font-family: mono;
+            font-weight: normal;
+            font-size: 11pt;
+            """)
 
     app.processEvents()
-
     extra = {'danger': '#dc3545',
              'warning': '#ffc107',
              'success': '#17a2b8',
              }
     apply_stylesheet(app, theme='dark_cyan.xml', extra=extra)
-    # apply_stylesheet(app, theme='light_cyan_500.xml',
-    # extra=extra, light_secondary=True)
 
     QIcon.setThemeName("icons-dark")
 
     frame = BCIFramework()
     frame.main.showMaximized()
 
-    if not splash is None:
+    if '--no_splash' in sys.argv:
         splash.finish(frame)
 
     app.exec_()
