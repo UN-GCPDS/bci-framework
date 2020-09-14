5f77c3198afe68ad0a11a3250049fc6f732376ac
Yeison <yeison.eng@gmail.com>
2020-09-13 23:12:13 -0500

    Add plotting

--- bci_framework/default_projects/Test/main.py
+++ bci_framework/default_projects/Test/main.py
@@ -2,11 +2,16 @@ from bci_framework.projects.figure import FigureStream
 from bci_framework.projects import properties as prop
 from bci_framework.projects.utils import loop_consumer, fake_loop_consumer
 
+from openbci_stream.utils import autokill_process
+
+autokill_process(name='testssss')
+
 import logging
 import numpy as np
 
-
 ########################################################################
+
+
 class Stream(FigureStream):
     """"""
 
@@ -15,37 +20,44 @@ class Stream(FigureStream):
         """"""
         super().__init__()
 
-        self.subsample = 32
+        self.L = 1000
         t = 30
 
-        self.axis = self.add_subplot(1, 1, 1)
-        self.tight_layout(rect=[0.03, 0.03, 1, 0.95])
+        self.create_buffer(t, aux_shape=3, fill=0)
 
-        lines = [self.axis.plot(np.zeros(100), np.zeros(100), '-')[0]
-                 for i in range(5)]
-        self.axis.set_xlim(-t, 0)
+        self.axis, self.time, self.lines = self.create_lines(mode='eeg', time=-t, window=self.L, cmap='cool')
 
-        self.axis.legend()
-
-        self.axis.set_title('External inputs')
+        self.axis.set_title('Raw EEG')
         self.axis.set_xlabel('Time [s]')
-        self.axis.set_ylabel('Amplitude')
+        self.axis.set_ylabel('Channel')
 
-        self.axis.grid(True)
-
-        # self.axis.set_aspect('auto')
+        # self.axis.set_ylim(0, len(prop.CHANNELS)+1)
+        self.axis.set_yticks(range(1, len(prop.CHANNELS) + 1))
+        self.axis.set_yticklabels(prop.CHANNELS.values())
 
         self.stream()
 
     # ----------------------------------------------------------------------
     @fake_loop_consumer
-    def stream(self, data, topic):
+    def stream(self, data, topic, frame):
         """"""
 
-        self.feed()
+        if topic == "eeg":
+
+            eeg = self.resample(self.buffer_eeg, self.L, axis=1)
+            eeg = self.centralize(eeg)
+
+            for i, line in enumerate(self.lines):
+                line.set_data(self.time, eeg[i] + i + 1)
+
+            # if not frame % 5:
+            logging.warning(f'feed! {frame} {prop.CHANNELS} {eeg.max()}')
+            self.feed()
+
+        elif topic == "marker":
+            data = data.value
+            logging.warning(data)
 
 
 if __name__ == '__main__':
     Stream()
-
-
