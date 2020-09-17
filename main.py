07ee65b87a0ff225ba7283b324ee56782801647b
Yeison <yeison.eng@gmail.com>
2020-09-17 14:10:45 -0500

    Added local directory for local dependencies

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # The purpose of this file is works as main entry point for debugger.
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'local'))
+
 from bci_framework.__main__ import main
 main()
+
+
