6d4731028c187a797ca05669f65c953a7cc8f10f
Yeison <yeison.eng@gmail.com>
2020-11-08 11:30:22 -0500

    Added a stop for the thread

--- bci_framework/bci_framework/nbstreamreader.py
+++ bci_framework/bci_framework/nbstreamreader.py
@@ -11,13 +11,14 @@ class NonBlockingStreamReader:
 
         self._s = stream
         self._q = Queue()
+        self.running = True
 
         def _populateQueue(stream, queue):
             '''
             Collect lines from 'stream' and put them in 'quque'.
             '''
 
-            while True:
+            while self.running:
                 line = stream.readline()
                 if line:
                     queue.put(line)
@@ -25,16 +26,22 @@ class NonBlockingStreamReader:
                     pass
                     # raise UnexpectedEndOfStream
 
-        self._t = Thread(target = _populateQueue,
-                args = (self._s, self._q))
+        self._t = Thread(target=_populateQueue,
+                         args=(self._s, self._q))
         self._t.daemon = True
-        self._t.start() #start collecting lines from the stream
+        self._t.start()  # start collecting lines from the stream
 
-    def readline(self, timeout = None):
+    def readline(self, timeout=None):
         try:
-            return self._q.get(block = timeout is not None,
-                    timeout = timeout)
+            return self._q.get(block=timeout is not None,
+                               timeout=timeout)
         except Empty:
             return None
+
+    # ----------------------------------------------------------------------
+    def stop(self):
+        """"""
+        self.running = False
+
 
 # class UnexpectedEndOfStream(Exception): pass
