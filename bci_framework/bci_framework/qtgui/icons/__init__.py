4d3f697b99f2e0a522f934d1c9e051bf4529c9c6
Yeison <yeison.eng@gmail.com>
2021-02-04 15:15:16 -0500

    cleared

--- bci_framework/bci_framework/qtgui/icons/__init__.py
+++ bci_framework/bci_framework/qtgui/icons/__init__.py
@@ -1,7 +1,7 @@
 import os
-from qt_material.resources import ResourseGenerator
 
 from PySide2.QtCore import QDir
+from qt_material.resources import ResourseGenerator
 
 
 # ----------------------------------------------------------------------
@@ -9,12 +9,15 @@ def generate_icons():
     """"""
     source = os.path.join(os.path.dirname(__file__), 'source')
     resources = ResourseGenerator(primary=os.getenv('QTMATERIAL_PRIMARYCOLOR'),
-                                  secondary=os.getenv('QTMATERIAL_SECONDARYCOLOR'),
-                                  disabled=os.getenv('QTMATERIAL_SECONDARYLIGHTCOLOR'),
+                                  secondary=os.getenv(
+                                      'QTMATERIAL_SECONDARYCOLOR'),
+                                  disabled=os.getenv(
+                                      'QTMATERIAL_SECONDARYLIGHTCOLOR'),
                                   source=source,
                                   parent='bci_framework',
                                   )
     resources.generate()
 
     QDir.addSearchPath('bci', resources.index)
-    QDir.addSearchPath('icons-dark', os.path.join(os.path.dirname(__file__), 'icons-dark'))
+    QDir.addSearchPath(
+        'icons-dark', os.path.join(os.path.dirname(__file__), 'icons-dark'))
