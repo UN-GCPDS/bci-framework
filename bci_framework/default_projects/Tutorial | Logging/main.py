598d50df42d752bb649b630a1e3260357dd59509
Yeison <yeison.eng@gmail.com>
2021-03-09 18:28:26 -0500

    Update logging example

--- bci_framework/default_projects/Tutorial | Logging/main.py
+++ bci_framework/default_projects/Tutorial | Logging/main.py
@@ -1,5 +1,10 @@
-from bci_framework.extensions.data_analysis import DataAnalysis
+from bci_framework.extensions.data_analysis import DataAnalysis, loop_consumer, fake_loop_consumer
 import logging
+
+logging.basicConfig()
+logging.root.name = "Analysis"
+logging.getLogger().setLevel(logging.DEBUG)
+
 
 ########################################################################
 class Analysis(DataAnalysis):
@@ -9,13 +14,19 @@ class Analysis(DataAnalysis):
     def __init__(self, *args, **kwargs):
         """"""
         super().__init__(*args, **kwargs)
-    
+        self.stream()
+            
+    # ----------------------------------------------------------------------
+    @fake_loop_consumer('eeg')
+    def stream(self):
+        """"""       
         logging.debug('debug')
         logging.info('info')
         logging.warning('warning')
         logging.error('error')
-        logging.critical('critical')        
-                    
+        logging.critical('critical')    
+         
+           
                                     
 if __name__ == '__main__':
     Analysis()
