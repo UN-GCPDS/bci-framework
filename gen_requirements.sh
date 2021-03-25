2aa7d20f52a5b4023331c6a61457325adaeed3b4
Yeison <yeison.eng@gmail.com>
2021-03-25 11:35:26 -0500

    Update requirements

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,1 +1,5 @@
-pipreqs --ignore bci_framework/default_projects,bci_framework/documentation,bci_framework/extensions/stimuli_delivery/path,bci_framework/kafka_scripts/latency_synchronization_stimuli_marker --savepath requirements.txt --force bci_framework
+pipreqs --ignore bci_framework/default_projects,bci_framework/documentation,bci_framework/extensions/stimuli_delivery/path,bci_framework/kafka_scripts/latency_synchronization_stimuli_marker --savepath requirements.tmp --force bci_framework
+rm requirements.txt
+sed '/kafka==/d' requirements.tmp >> requirements.txt
+rm requirements.tmp
+cat requirements.txt
