24214ea7dc37ac7390bb54da517cde7ae00468ce
Yeison <yeison.eng@gmail.com>
2021-04-01 11:38:14 -0500

    typo

--- bci_framework/extensions/__init__.py
+++ bci_framework/extensions/__init__.py
@@ -5,7 +5,7 @@ Extensions
 
 The BCI-Framework extensions consist of a feature that brings to the user the
 capabilities to develop real-time neurophysiological experiments with remote
-stimuli delivery and EEG ``events`` synchronization and real-time data analysis
+stimuli delivery and EEG events synchronization and real-time data analysis
 and visualizations.
 
 """
