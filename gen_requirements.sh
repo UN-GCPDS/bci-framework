dc958988a3fb7703cae76911090a39f2d8b84c51
Yeison <yeison.eng@gmail.com>
2021-04-21 16:37:12 -0500

    Update requirements

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,6 +1,7 @@
-pipreqs --ignore bci_framework/default_projects,bci_framework/documentation,bci_framework/extensions/stimuli_delivery/path,bci_framework/kafka_scripts/latency_synchronization_stimuli_marker --savepath requirements.tmp --force bci_framework
 rm requirements.txt
-sed '/kafka==/d' requirements.tmp >> requirements.txt
-rm requirements.tmp
-sed -i 's/==/>=/' requirements.txt
-cat requirements.txt
+pipreqs --ignore bci_framework/default_projects,bci_framework/documentation,bci_framework/extensions/stimuli_delivery/path,bci_framework/kafka_scripts/latency_synchronization_stimuli_marker --savepath requirements.txt --force bci_framework
+sed -i '/kafka==/d' requirements.txt
+sed -i '/browser==/d' requirements.txt
+sed -i '/seaborn==/d' requirements.txt
+sed -i 's/==.*//' requirements.txt
+python -c "[print(f'\'{line[:-1]}\',') for line in open('requirements.txt').readlines()]"
