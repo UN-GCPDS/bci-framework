debdc597b5a86713914188e62632c51c4eb5fead
Yeison Cardona <yencardonaal@unal.edu.co>
2023-06-21 01:00:59 -0500

    Add Docker

--- docker/bci-framework.sh
+++ docker/bci-framework.sh
@@ -2,4 +2,5 @@
 
 sudo chmod -R 777 /home/$USER/.bciframework_docker
 xhost +local:docker
+docker pull dunderlab/bci_framework:latest
 docker run --network=host --privileged -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri -v /home/$USER/.bciframework_docker:/home/user/.bciframework dunderlab/bci_framework
